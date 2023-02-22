8ba29aeb8c8f916f1330a243203118c3829a8945
vivicastilho <vivianegomes.dev@gmail.com>
2023-02-22 14:20:29 -0300

    Global Style

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    NAV_BACKGROUND: '#232129',
+    BACKGROUND: '#312E38',
+
+    FONT_BLACK: '#333333',
+    TEXT_COLOR: '#999591',
+
+    LINE_COLOR: '#3E3B47',
+
+    PRIMARY_COLOR: '#B03964',
+  },
+};
